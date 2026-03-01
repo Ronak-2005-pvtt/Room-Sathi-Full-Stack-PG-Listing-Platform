42e5a7dbe3b2963aae4bc2e82405431757e0dec5
Ronak-2005-pvtt <181397246+Ronak-2005-pvtt@users.noreply.github.com>
2026-03-01 15:31:36 +0530

    Safe image render fix

--- routes/listing.js
+++ routes/listing.js
@@ -5,17 +5,19 @@ const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer  = require('multer')
-// const {storage} = require("../cloudConfig.js"); 
-// const upload = multer({ storage })
+const {storage} = require("../cloudConfig.js"); 
+const upload = multer({ storage })
 
 router
   .route("/")
   .get(wrapAsync(listingController.index))
   .post(
-  isLoggedIn,
-  validateListing,
-  wrapAsync(listingController.createListing),
-);
+    isLoggedIn,
+    upload.single('listing[image]'),
+    validateListing,  
+    wrapAsync(listingController.createListing),
+    
+  );
 
 //New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
@@ -24,11 +26,12 @@ router
   .route("/:id")
   .get(wrapAsync(listingController.showListing))
   .put(
-  isLoggedIn,
-  isOwner,
-  validateListing,
-  wrapAsync(listingController.updateListing),
-)
+    isLoggedIn,
+    isOwner,
+    upload.single('listing[image]'),
+    validateListing,
+    wrapAsync(listingController.updateListing),
+  )
   .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 //Edit Route
