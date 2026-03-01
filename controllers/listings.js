9a68844806a5b0bf313f1700ab521902c44cd3a9
Ronak-2005-pvtt <181397246+Ronak-2005-pvtt@users.noreply.github.com>
2026-03-01 15:59:36 +0530

    Safe image render fix

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,121 +1,141 @@
-const Listing = require("../models/listing.js");
-const axios = require("axios");
+  const Listing = require("../models/listing.js");
+  const axios = require("axios");
 
-module.exports.renderNewForm = (req, res) => {
-  res.render("listings/new.ejs");
-};
+  // Render New Form
+  module.exports.renderNewForm = (req, res) => {
+    res.render("listings/new.ejs");
+  };
 
-module.exports.showListing = async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id)
-    .populate({
-      path: "reviews",
-      populate: {
-        path: "author",
-      },
-    })
-    .populate("owner");
-  if (!listing) {
-    req.flash("error", "Listing you requested for does not exist!");
-    return res.redirect("/listings");
-  }
-  console.log(listing);
-  res.render("listings/show.ejs", { listing });
-};
+  // Show Listing
+  module.exports.showListing = async (req, res) => {
+    const { id } = req.params;
 
-module.exports.createListing = async (req, res, next) => {
-  try {
-    const { location } = req.body.listing;
+    const listing = await Listing.findById(id)
+      .populate({
+        path: "reviews",
+        populate: { path: "author" },
+      })
+      .populate("owner");
 
-    const geoUrl = `https://api.maptiler.com/geocoding/${encodeURIComponent(location)}.json?key=${process.env.MAP_KEY}`;
-
-    const response = await axios.get(geoUrl);
-
-    if (!response.data.features || response.data.features.length === 0) {
-      req.flash("error", "Invalid location");
-      return res.redirect("/listings/new");
+    if (!listing) {
+      req.flash("error", "Listing does not exist!");
+      return res.redirect("/listings");
     }
 
-    const coordinates = response.data.features[0].geometry.coordinates;
+    res.render("listings/show.ejs", { listing });
+  };
 
-    if (!coordinates || coordinates.length !== 2) {
-      req.flash("error", "Coordinates not found");
-      return res.redirect("/listings/new");
+  // Create Listing
+  module.exports.createListing = async (req, res, next) => {
+    try {
+      const { location } = req.body.listing;
+
+      const geoUrl = `https://api.maptiler.com/geocoding/${encodeURIComponent(location)}.json?key=${process.env.MAP_KEY}`;
+      const response = await axios.get(geoUrl);
+
+      if (!response.data.features || response.data.features.length === 0) {
+        req.flash("error", "Invalid location");
+        return res.redirect("/listings/new");
+      }
+
+      const coordinates = response.data.features[0].geometry.coordinates;
+
+      const newListing = new Listing(req.body.listing);
+      newListing.owner = req.user._id;
+
+      // SAFE IMAGE
+      if (req.file) {
+        newListing.image = {
+          url: req.file.path,
+          filename: req.file.filename,
+        };
+      } else {
+        newListing.image = {
+          url: "https://via.placeholder.com/600x400",
+          filename: "default",
+        };
+      }
+
+      newListing.geometry = {
+        type: "Point",
+        coordinates: coordinates,
+      };
+
+      await newListing.save();
+
+      req.flash("success", "New Listing Created!");
+      res.redirect(`/listings/${newListing._id}`);
+
+    } catch (err) {
+      next(err);
     }
+  };
 
-    let url = req.file.path;
-    let filename = req.file.filename;
+  // Edit Form
+  module.exports.renderEditForm = async (req, res) => {
+  const { id } = req.params;
 
-    const newListing = new Listing(req.body.listing);
-    newListing.owner = req.user._id;
-    newListing.image = { url, filename };
-
-    newListing.geometry = {
-      type: "Point",
-      coordinates: coordinates,
-    };
-
-    await newListing.save();
-
-    req.flash("success", "New Listing Created!");
-    res.redirect(`/listings/${newListing._id}`);
-
-  } catch (err) {
-    next(err);
-  }
-};
-
-module.exports.renderEditForm = async (req, res) => {
-  let { id } = req.params;
   const listing = await Listing.findById(id);
   if (!listing) {
-    req.flash("error", "Listing you requested for does not exist!");
+    req.flash("error", "Listing does not exist!");
     return res.redirect("/listings");
   }
-  let originalImageUrl = listing.image.url;
-  originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
-  res.render("listings/edit.ejs", { listing , originalImageUrl});
-};
 
-module.exports.updateListing = async (req, res) => {
-  let { id } = req.params;
-  let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  let originalImageUrl = listing.image?.url;
 
-  if(typeof req.file !== "undefined") {
-  let url = req.file.path;
-  let filename = req.file.filename;
-  listing.image = {url, filename};
-  await listing.save();
+  if (originalImageUrl) {
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
   }
-  req.flash("success", "Listing Updated!");
-  res.redirect(`/listings/${id}`);};
 
-module.exports.destroyListing = async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  req.flash("success", "Listing Deleted!");
-  res.redirect("/listings");
+  res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
-module.exports.index = async (req, res) => {
-  let { search } = req.query;
-  let allListings;
+  // Update Listing
+  module.exports.updateListing = async (req, res) => {
+    const { id } = req.params;
 
-  if (search) {
-    allListings = await Listing.find({
-      $or: [
-        { title: { $regex: search, $options: "i" } },
-        { location: { $regex: search, $options: "i" } },
-        { country: { $regex: search, $options: "i" } }
-      ]
+    const listing = await Listing.findByIdAndUpdate(id, {
+      ...req.body.listing,
     });
-  } else {
-    allListings = await Listing.find({});
-  }
 
-  res.render("listings/index.ejs", { 
-    allListings,
-    search
-  });
-};
+    if (req.file) {
+      listing.image = {
+        url: req.file.path,
+        filename: req.file.filename,
+      };
+      await listing.save();
+    }
+
+    req.flash("success", "Listing Updated!");
+    res.redirect(`/listings/${id}`);
+  };
+
+  // Delete Listing
+  module.exports.destroyListing = async (req, res) => {
+    const { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+
+    req.flash("success", "Listing Deleted!");
+    res.redirect("/listings");
+  };
+
+  // Index
+  module.exports.index = async (req, res) => {
+    const { search } = req.query;
+
+    let allListings;
+
+    if (search) {
+      allListings = await Listing.find({
+        $or: [
+          { title: { $regex: search, $options: "i" } },
+          { location: { $regex: search, $options: "i" } },
+          { country: { $regex: search, $options: "i" } },
+        ],
+      });
+    } else {
+      allListings = await Listing.find({});
+    }
+
+    res.render("listings/index.ejs", { allListings, search });
+  };
